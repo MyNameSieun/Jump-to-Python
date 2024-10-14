34c2a6f0a9bc97f819a4493682ca20898c89d717
sieun <charm516@naver.com>
2024-10-14 16:12:02 +0900

    [feat] - 문자열 포매팅 예제 추가

--- practice.py
+++ practice.py
@@ -49,3 +49,52 @@ print(a)
 
 a="%-10s"%"hi"
 print(a)
+
+a="%10sGood"%"hi"
+print(a) 
+
+a="%-10sGood"%"hi"
+print(a)
+
+
+# 소수점 표현하기
+a="%0.4f"%3.42324
+print(a)# 3.4232
+
+a="%.4f"%3.42324
+print(a)
+
+a="%10.4f"%3.42324
+print(a) #   3.4232
+
+a="I eat {0} {1}".format("감자",3)
+print(a)
+
+number=3
+a="I eat {0}".format(number)
+print(a)
+
+number=10
+string="5"
+a="I {number} {string}".format(number=3,string="sdf")
+print(a) 
+
+
+number=14
+a="I {0} {stw}".format(number,stw="4")
+print(a)
+
+
+a="{0:^10}".format("감자")
+print(a)
+
+a="{0:!^10}".format("기러기")
+print(a)
+
+a="{0:=<10}".format("등호!")
+print(a)
+
+a="{0:#^10}".format("안녕!")
+print(a)
+
+
