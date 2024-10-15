fa8ff82a0c6a8461fd4f252d4c4ff4d0f910af15
sieun <charm516@naver.com>
2024-10-15 16:45:53 +0900

    [feat] - 문제 추가

--- practice.py
+++ practice.py
@@ -1,40 +1,6 @@
-s1=set([1,2,3,4,5,6])
-s2=set([4,5,6,7,8,9])
-
-
-#교집합
-a=s1&s2
-b=s1.intersection(s2)
+a = "%10s" % "apple"
 print(a)
-print(b)
 
-# 합집합
-a=s1|s2
-b=s1.union(s2)
+a = "%-10s" % "apple"
 print(a)
-print(b)
 
-# 차집합
-a=s1-s2
-b=s1.difference(s2)
-print(a)
-print(b)
-
-a=s2-s1
-b=s2.difference(s1)
-print(a)
-print(b)
-
-s1=set([1,2,3])
-s1.add(4)
-print(s1)
-
-
-s1=set([1,2,3])
-s1.update([4,5,6])
-print(s1)
-
-
-s1=set([1,2,3])
-s1.remove(3)
-print(s1 )
